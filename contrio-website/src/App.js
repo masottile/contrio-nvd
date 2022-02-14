8a6a19d9ac18d26fd4785c130016be1b7ce5316e
Austin Cho <austin.cho14@gmail.com>
2022-02-14 15:06:50 -0800

    add navigation bar and log in and log out functionality (#8)
    * add navigation bar and log in and log out functionality
    
    * add instructions
    
    Co-authored-by: Austin Cho <austincho@Austins-MacBook-Air.local>

--- contrio-website/src/App.js
+++ contrio-website/src/App.js
@@ -1,16 +1,30 @@
-import Header from './components/Header'
-import Form from './components/Form'
+import Homepage from './pages/Homepage';
+import ContractsPage from './pages/ContractsPage';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import NavigationBar from './components/NavigationBar'
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
-      <>
-        <Header title = 'Contrio'/>
-        <Form type={'input'}/>
-        <Form type={'output'}/>
-      </>
+      <Router>
+        <NavigationBar/>
+        <Routes>
+          <Route path='/contracts' element={<ContractsPage/>}/>
+          <Route exact path='/' element={<Homepage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ contrio-website/src/pages/ContractsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from '../components/Form'
+  
+function ContractsPage(){
+    return (
+        <>
+            <Form type={'input'}/>
+            <Form type={'output'}/>
+        </>
+    );
+}
+
+export default ContractsPage;
